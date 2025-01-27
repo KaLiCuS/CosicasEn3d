cdf770e8b1d74f4706f62be42dea2709d1780b22
Daniel Pérez <1989danielperez1989@gmail.com>
2025-01-27 18:08:09 +0100

    Revert "Nuevo commit"
    This reverts commit 740778bd7420a0a7e5af5f5eb1d20be9862cbfd9.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -154,7 +154,6 @@
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-<<<<<<< HEAD
     <script>
         // Script para mostrar la imagen en el modal
     document.addEventListener("DOMContentLoaded", function() {
@@ -199,8 +198,6 @@
                 alert("Error: " + error.message);
             });
         });
-=======
->>>>>>> 031b740 (Nuevo commit)
     </script>
 </body>
 </html>
