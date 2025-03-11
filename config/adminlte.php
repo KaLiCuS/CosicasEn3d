1c0518138456168a39af4aa0c361a9a1a4364f53
Daniel Pérez <1989danielperez1989@gmail.com>
2025-03-11 23:25:55 +0100

    CosicasEn3DDDani

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>CosicasEn3D</b>',
+    'logo' => '<b>CosicasEn3Dani</b>',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
